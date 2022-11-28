60bf73584cf3ec85d38bf15e3182b88b53924dfe
hyesu.son <46313348+sduu@users.noreply.github.com>
2022-11-28 15:58:33 +0900

    add : 2022-11-28

---
+++ React/프로젝트_2/my-app/src/App.js
@@ -0,0 +1,25 @@
+import {useState, useEffect} from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import UserContext from "./context/UserContext";
+// import {UserContext} from "./context/UserContext";
+import BlogDetail from "./pages/BlogDetail/BlogDetail";
+import Home from "./pages/Home/Home";
+import "./reset.css";
+import "./global.css";
+
+function App() {
+    const [userId, setUserId] = useState(1);
+    const [isLogin, setIsLogin] = useState(true);
+
+    return (
+        <UserContext.Provider value={{userId, isLogin}}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/blog/:id" element={<BlogDetail />} />
+                </Routes>
+            </BrowserRouter>
+        </UserContext.Provider>
+    );
+}
+export default App;
---
+++ React/프로젝트_2/my-app/src/components/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+import "./banner.css";
+
+const Banner = () => {
+    return (
+        <>
+            {/* banner */}
+            <div className="banner">
+                <div className="max-width">
+                    <div className="banner-contents">
+                        <p className="sub-text">React Blog</p>
+                        <p className="main-text">My BLOG</p>
+                        <p className="description">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Officiis nostrum dolor provident beatae
+                            deserunt magnam, illo neque. Maiores ipsum commodi
+                            dolorum beatae aliquid laboriosam est
+                        </p>
+                    </div>
+                </div>
+            </div>
+            {/* //banner */}
+        </>
+    );
+};
+
+export default Banner;
